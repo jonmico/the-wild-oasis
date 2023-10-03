c7eeeed98e714f78fa7793cb4ff2f947aa96624c
Jon Mico <jonmico@gmail.com>
2023-10-03 18:23:34 -0400

    Break up some styled components into different files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -31,11 +16,14 @@ const StyledApp = styled.div`
 
 export default function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert('Hi, Mom.')}>Check In</Button>
-      <Button onClick={() => alert('Bye, Mom.')}>Check Out</Button>
-      <Input type={'number'} placeholder={'Number of guests'} />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Hi, Mom.')}>Check In</Button>
+        <Button onClick={() => alert('Bye, Mom.')}>Check Out</Button>
+        <Input type={'number'} placeholder={'Number of guests'} />
+      </StyledApp>
+    </>
   );
 }
