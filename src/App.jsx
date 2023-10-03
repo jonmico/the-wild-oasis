b7418c63c6f1a66317cd32ba8493257f2bbfabf4
Jon Mico <jonmico@gmail.com>
2023-10-03 19:04:59 -0400

    The buttons are now changing with props.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -14,12 +15,29 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading $as={'h1'}>The Wild Oasis</Heading>
-        <Heading $as={'h2'}>Check in and out</Heading>
-        <Button onClick={() => alert('Hi, Mom.')}>Check In</Button>
-        <Button onClick={() => alert('Bye, Mom.')}>Check Out</Button>
-        <Heading $as={'h3'}>Form</Heading>
-        <Input type={'number'} placeholder={'Number of guests'} />
+        <Row>
+          <Row $type={'horizontal'}>
+            <Heading $as={'h1'}>The Wild Oasis</Heading>
+            <div>
+              <Heading $as={'h2'}>Check in and out</Heading>
+              <Button onClick={() => alert('Hi, Mom.')}>Check In</Button>
+              <Button
+                $variation={'secondary'}
+                $size={'small'}
+                onClick={() => alert('Bye, Mom.')}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading $as={'h3'}>Form</Heading>
+            <form>
+              <Input type={'number'} placeholder={'Number of guests'} />
+              <Input type={'number'} placeholder={'Number of guests'} />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
